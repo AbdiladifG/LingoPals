fb5426f4a6964b94716081efac790a8e90634c38
abdiladifgurhan <abdiladifgurhan@gmail.com>
2024-11-19 18:13:05 -0500

    add config back

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://abdiladifgurhan:KpiS1xf3cm4IGNPX@cluster0.6t2kdm9.mongodb.net/demoday?retryWrites=true&w=majority&appName=Cluster0'
+};
